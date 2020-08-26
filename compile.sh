19176e79da03dac5bd21ad3eb9f5895931e56cd9
Jay White <JayWhite2357@users.noreply.github.com>
2020-08-26 08:24:14 -0400

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,4 +1,1 @@
-./node_modules/browserify/bin/cmd.js WeBWorKConverter.js -o WW2ClibBig.js
-#./node_modules/minify/bin/minify.js WW2ClibBig.js > WW2Clib.js
-cat WW2ClibBig.js > WW2Clib.js
-cp WW2Clib.js main.html ~/Downloads
+./node_modules/browserify/bin/cmd.js WeBWorKConverter.js -o WW2Clib.js
